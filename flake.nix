d3cc85f0f66c417d92ef25cb9dbeecf65d05d48e
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2026-01-05 16:07:37 +0100

    nicer overrides, I hope

--- flake.nix
+++ flake.nix
@@ -5,52 +5,20 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {self, nixpkgs,flake-utils}:
+
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages = rec {
-            nixos-splash-plasma6 = pkgs.stdenv.mkDerivation rec {
-              pname = "nixos-splash-plasma6";
-              version = "1.0";
-              src = ./.;
-              
-              # Allow overriding the splash text
-              splashText = "Plasma made by KDE · flake default";
-              
-              dontBuild = true;
-              
-              installPhase = ''
-                mkdir -p $out/share/plasma/look-and-feel/NixOS-Splash-Plasma6
-                cp -r . $out/share/plasma/look-and-feel/NixOS-Splash-Plasma6
-                
-                # Substitute the splash text in Splash.qml
-                substituteInPlace $out/share/plasma/look-and-feel/NixOS-Splash-Plasma6/contents/splash/Splash.qml \
-                  --replace-fail '"Plasma made by KDE"' '"${splashText}"'
-              '';
-            };
-            default = nixos-splash-plasma6;
-          };
-    }
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          # Package with a nice override interface:
+          #   inputs.plasma-splash.packages.${system}.default.override { splashText = "..."; }
+          nixos-splash-plasma6 = pkgs.callPackage ./nixos-splash-plasma6.nix { };
+
+          default = nixos-splash-plasma6;
+        };
+      }
     );
 }
-  # outputs = { self, nixpkgs, flake-utils }: {
-  #   flake-utils.lib.eachDefaultSystem (system:
-  #     let pkgs = nixpkgs.legacyPackages.${system};
-  #     in {
-  #     packages = rec {
-  #       nixos-splash-plasma6 = pkgs.stdenv.mkDerivation {
-  #         pname = "nixos-splash-plasma6";
-  #         version = "1.0";
-  #         src = ./.;
-  #         dontBuild = true;
-  #         installPhase = ''
-  #           mkdir -p $out/share/plasma/look-and-feel/NixOS-Splash-Plasma6
-  #           install ./contents $out/share/plasma/look-and-feel/NixOS-Splash-Plasma6
-  #         '';
-  #       };
-  #       default = nixos-splash-plasma6;
-  #     };
-  #   }
-  #   );
-  # }
